602718465084b90efc3147235a3ae209ab8e288f
Owen <peiwencao@qq.com>
2022-10-30 22:39:38 +0800

    第一个案例，一个无聊的钟表

--- C01 Boing Clock/Assets/Scripts/Clock.cs
+++ C01 Boing Clock/Assets/Scripts/Clock.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Clock : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform hourHand, minuteHand, secondHand;
+
+    private void Awake()
     {
-        
+        DateTime now = DateTime.Now;
+        float s = 6f * (now.Second + now.Millisecond * 0.001f);
+        float m = 6f * (now.Minute + now.Second / 60f);
+        float h = 30f * (now.Hour + now.Minute / 60f);
+
+        secondHand.localRotation = Quaternion.Euler(s, 0, 0);
+        minuteHand.localRotation = Quaternion.Euler(m, 0, 0);
+        hourHand.localRotation = Quaternion.Euler(h, 0, 0);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        DateTime now = DateTime.Now;
+        float s = 6f * (now.Second + now.Millisecond * 0.001f);
+        float m = 6f * (now.Minute + now.Second / 60f);
+        float h = 30f * (now.Hour + now.Minute / 60f);
+
+        secondHand.localRotation = Quaternion.Euler(s, 0, 0);
+        minuteHand.localRotation = Quaternion.Euler(m, 0, 0);
+        hourHand.localRotation = Quaternion.Euler(h, 0, 0);
     }
 }
